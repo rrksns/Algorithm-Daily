381c297b56cb618613579272fd47de870fc86ce1
seol <rrksns@gmail.com>
2026-04-11 16:47:50 +0900

    구현 : #1789 : 수학 구현 : #11651 : 정렬

---
+++ 260411/11651.py
@@ -0,0 +1,26 @@
+# import sys
+# n=int(input())
+# li=[]
+# for i in range(n):
+#     a, b = map(int,sys.stdin.readline().split())
+#     li.append([a,b])
+# li.sort(key=lambda x: (x[1], x[0]))
+# for i in li:
+#     print(i[0], i[1])
+
+
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+array = []
+for i in range(n):
+    x, y = map(int, input().split())
+    array.append([y, x])
+
+s_array = sorted(array)
+
+for y, x in s_array:
+    print(x, y)
